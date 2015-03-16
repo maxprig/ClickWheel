4290c35642cc9b07f5ce229d297e735695f5a133
doertydoerk <dirk@gerretz.de>
2015-03-16 13:17:11 +0100

    Initial Commit

---
+++ ClickWheel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClickWheel
+//
+//  Created by Dirk Gerretz on 16/03/15.
+//  Copyright (c) 2015 [code2app];. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
